7deb523c966649258dc823627878db3caae74a5c
haykam821 <haykam821@users.noreply.github.com>
2017-09-21 07:18:03 -0400

    Project shell!

--- Units.obp.js
+++ Units.obp.js
@@ -1,1 +1,9 @@
+exports.onMessageReceived = (function Units(bot, doc, user, userID, channelID, message, event) {
+  require('./../exports.js').registerCmd(['convert <value> <unit> <toUnit>'], 'Converts a value from one unit to another.');
 
+  if (message === undefined) {
+    return;
+  }
+  if (message.startsWith(doc.prefix + "convert ") {
+  }
+});
