0388a2e8265e4636625f2a02abd1cd47d74bf484
haykam821 <haykam821@users.noreply.github.com>
2017-09-21 22:32:57 -0400

    It should work now...?

--- Units.obp.js
+++ Units.obp.js
@@ -7,5 +7,29 @@ exports.onMessageReceived = (function Units(bot, doc, user, userID, channelID, m
     return;
   }
   if (message.startsWith(doc.prefix + "convert ") {
+    bot.simulateTyping(channelID);
+
+    var arguments = message.replace(doc.prefix + "convert ", "").split(" ");
+    var converted = convert(arguments[0]).from(arguments[1]).to(arguments[2]);
+  
+    bot.sendMessage({
+      to: channelID,
+        embed: {
+          title: "Converted",
+          color: 0x77b255,
+          timestamp: new Date(),
+          description: ":white_check_mark: The value was succesfully converted.",
+          fields: [
+            {
+              name: "Input",
+              value: `${argument[0]} ${argument[1]}`
+            },
+            {
+              name: "Output",
+              value: `${converted} ${argument[2]}`
+            }
+          ]
+        }
+    });
   }
 });
